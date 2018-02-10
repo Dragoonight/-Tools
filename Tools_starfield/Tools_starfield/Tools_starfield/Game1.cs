8a60dad7c2d125ca5f929905ca82e4c6d738b1cf
Unknown <drakmusse.musse123@gmail.com>
2018-02-10 17:19:48 +0100

    Så det ser ut som det fungerar #working #relatable

--- Tools_starfield/Tools_starfield/Tools_starfield/Game1.cs
+++ Tools_starfield/Tools_starfield/Tools_starfield/Game1.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+//The differnce
 namespace Tools_starfield
 {
     /// <summary>
